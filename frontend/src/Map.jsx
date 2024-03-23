abf2d703a406393d34515dadb249a8bccebd9feb
MartinovG <gabismart7@gmail.com>
2024-03-23 21:20:11 +0200

    -

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -84,5 +84,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'ApiKeyGoesHere'
+  apiKey: 'AIzaSyBfMGCbFUHFAeVhvIWAFHOkRbNS9JcCNNc'
 })(MapContainer);
